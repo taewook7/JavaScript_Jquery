306a110ccfb87028edaca9330d92520518c3b0ac
taewook kim <x_o_d_n@naver.com>
2020-09-29 22:27:42 +0900

    Add files via upload

--- 0925 hubble/js/custom.js
+++ 0925 hubble/js/custom.js
@@ -29,7 +29,58 @@ $( document ).ready(function() {
     //     $(".panel").eq(index).show();
     //     // 디스플레이가 none 일때 쇼로 설정하면 버튼을 클릭할떄 나오고 디스플레이가 블록일때 하이드를 누르면 숨겨진다.
         
-   
-    
+   // about section tabs
+   var aboutTabs =function() {
+    $(".aboutTab .tab .commonBtn").click(function(){
+    var tabIndex =   $(this).parent().parent().parent().index();
+    // alert(tabIndex);
+    $(".panelBox").hide();
+        $(".panelBox").eq(tabIndex).show();
+    });
+
+    $(".aboutTab .tab").eq(0).find(".commonBtn").trigger("click"); 
+    // 시작하자마자 eq 0번째를 클릭 
+   }
+
+    aboutTabs();
+
+
+   //header fix on browser 
+   var headerFix= function() {
+        $(window).scroll(function() {
+            var scroll=$(window).scrollTop();
+            console.log(scroll);
+
+
+            for(var i=0; i<$("section").length; i++) {
+                var sectionTop=$("section").eq(i).offset().top;
+                var sectionBottom=sectionTop+$("section").eq(i).outerHeight();
+
+                console.log(sectionTop);
+                console.log(sectionBottom);
+            }
+
+            if(scroll > 0) {
+                // $("header").css({position:'fixed',height:'100px' });
+                $("header").addClass('on');
+            }else {
+                // $("header").css({position:'static'});
+                $("header").removeClass('on');
+            }
+        });
+   }
+   headerFix();
+
+   var moveSection=function() {
+        $(".gnb li").click(function(){
+            var index=$(this).index();
+            var sectionTop =$("section").eq(index).offset().top;
+            // alert(index);
+            $("html,body").animate({scrollTop:sectionTop});
+        });
+   }
+
+   moveSection();
+//    console.log($("section").eq(1).offset().top);
 
 });
